0f5677f4a30c94382a71bb88eaa0bb1edce264af
yusuf <horiyorrmi72@gmail.com>
2024-10-23 12:03:30 +0100

    Structured Query Language Scripts and dumps

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,3 @@
+-- Structured Query Language script that creates a trigger
+CREATE TRIGGER decrease_qty AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
